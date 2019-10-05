5b03e5909f2d4291e5efb31c8e1d15055b3b14fb
Artem Buyanov <russinvad@gmail.com>
2019-10-06 00:28:42 +0300

    done sendForm

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,5 +1,53 @@
 const sendForm = () => {
+    const errorMessage = 'Извините, что-то пошло не так',
+        loadMessage = 'Идёт отправка, ожидайте',
+        succesMessage = 'Отправлено';
 
+    const statusMessage = document.createElement('div');
+    statusMessage.style.cssText = 'font-size: 22px';
+
+    document.body.addEventListener('submit', (event) => {
+        event.preventDefault();
+        event.target.appendChild(statusMessage);
+
+        statusMessage.textContent = loadMessage;
+        
+        const formData = new FormData(event.target);
+
+        postData(formData)
+            .then((response) => {
+                if(response.status !== 200){
+                    throw new Error('Status network not 200');
+                }
+                statusMessage.textContent = succesMessage;
+            })
+            .then(() => event.target.querySelectorAll('input').forEach((item) => {
+                item.value = '';  
+            }))
+            .catch(() => statusMessage.textContent = errorMessage);
+    });
+
+    document.body.addEventListener('input', (event) => {
+        let target = event.target;
+
+        if (target.classList.contains('phone-user')){
+            target.value = target.value.replace(/[^0-9]/, '');
+        }
+        if (target.getAttribute('name') === 'user_name'){
+            target.value = target.value.replace(/[^А-яЁё]/, '');
+        }
+    });
+
+    const postData = (formData) => {
+        return fetch('./server.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: formData,
+            credentials: 'include'
+        });
+    };
 };
 
 export default sendForm;
