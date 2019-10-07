c0d3a74ddbf28d9c80986f056c38603802fe3dbd
Artem Buyanov <russinvad@gmail.com>
2019-10-07 22:19:45 +0300

    feat accord

--- src/modules/accord.js
+++ src/modules/accord.js
@@ -1,5 +1,31 @@
 const accord = () => {
+    const accordion = document.querySelector('.slideInDown'),
+        panelBtn = document.querySelectorAll('.panel-heading'),
+        content = document.querySelectorAll('.collapse');
+        console.log(panelBtn);
 
+    const accordContent = (index) => {
+        content.forEach((item, i) => {
+            if(index === i){
+                item.style.cssText = 'display: block';
+            }else{
+                item.style.cssText = 'display: none';
+            }
+        });
+    };
+    
+    accordion.addEventListener('click', (event) => {
+        let target = event.target;
+        event.preventDefault();
+        target = target.closest('.panel-heading');
+        if(target){
+            panelBtn.forEach((item, i) => {
+                if(item === target){
+                    accordContent(i);
+                }
+            });
+        }
+    });
 };
 
 export default accord;
