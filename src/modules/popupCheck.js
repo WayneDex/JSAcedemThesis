06055ca7a862250295525b12f40b7ecaba1631ff
Artem Buyanov <russinvad@gmail.com>
2019-10-08 00:11:15 +0300

    done popupCheck

--- src/modules/popupCheck.js
+++ src/modules/popupCheck.js
@@ -1,5 +1,36 @@
 const popupCheck = () => {
+    const checkBtn = document.querySelector('.popup'),
+    popupCheckContent = document.querySelector('.popup-content');
 
+    document.body.addEventListener('click', (event) => {
+        let target = event.target;
+        
+        if (target.closest('.check-btn')) {
+            event.preventDefault();
+            let popupWindow = document.documentElement.clientWidth;
+            
+            if (popupWindow >= 750) {
+                checkBtn.style.cssText = 'display: block';
+                popupCheckContent.animate([
+                    {transform: 'translateX(1000px)'},
+                    {transform: 'translateX(-200px)'}
+                ], {
+                    duration: 750,
+                    iterations: 1
+                });
+            } else {
+                checkBtn.style.cssText = 'display: block';
+            }
+        } else if (target.classList.contains('popup-close')) {
+            checkBtn.style.cssText = 'display: none';
+        } else {
+            let popup = target.closest('.popup-content');
+
+            if (!popup) {
+                checkBtn.style.cssText = 'display: none';
+            }
+        }
+    });
 };
 
 export default popupCheck;
